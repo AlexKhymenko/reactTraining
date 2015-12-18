f204600d0c13dadd2f60595d36b6112618f0babb
AlexKhymenko <alexanderkhymenko@gmail.com>
2015-12-18 17:45:37 +0200

    Work with React.Children.only To remember if u pass 2 children it will fail

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,8 +8,8 @@ let yeomanImage = require('../images/yeoman.png');
 class AppComponent extends React.Component {
 
   render() {
-    var count = React.Children.count(this.props.children);
-    console.log('count', count);
+    var count = React.Children.only(this.props.children);
+    console.log('child', count);
     return (
       <div>
         {this.props.name}
--- src/components/run.js
+++ src/components/run.js
@@ -6,5 +6,4 @@ import App from './Main';
 // Render the main component into the dom
 ReactDOM.render(<App name="nice">
   <p key="firsty">a child</p>
-  <p key="2">another</p>
 </App>, document.getElementById('app'));
