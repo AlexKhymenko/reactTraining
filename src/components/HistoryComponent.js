cfd95e947bdcb17fd85b0d0880499ace6d8998a8
AlexKhymenko <alexanderkhymenko@gmail.com>
2015-12-20 17:42:09 +0200

    Add history Component

---
+++ src/components/HistoryComponent.js
@@ -0,0 +1,54 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//History.less');
+
+class ListItem extends React.Component {
+  render() {
+    return <li>{this.props.name} - {this.props.result}</li>;
+  }
+}
+
+class History extends React.Component {
+  var _mockHistory =
+    [
+      {
+        "name": "Murph",
+        "result": "32:18",
+        "notes": "painful, but fun"
+      },
+      {
+        "name": "Tabata Something Else",
+        "type": "reps",
+        "result": "421",
+        "notes": ""
+      }];
+  render() {
+    var hist = this._mockHistory;
+    var formatedLi = [];
+    hist.forEach(function(object) {
+      var histObj = {
+        name: object.name,
+        result: object.result
+      };
+      formatedLi.push(<ListItem {...histObj}/>);
+    });
+
+    return(
+      <div>
+        <h2>History</h2>
+        <ul>
+          {formatedLi}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+// Uncomment properties you need
+// HistoryComponent.propTypes = {};
+// HistoryComponent.defaultProps = {};
+
+export default History;
